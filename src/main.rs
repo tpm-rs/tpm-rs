9b242a2552deed900348ce7426de0d3df8ddac0c
Robert Schaedler <rschaedler@microsoft.com>
2023-07-24 21:49:28 +0000

    add basic test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, world!");
+
+    let a = 1;
+    let b = 2;
+    let c = add(a, b);
+    println!("{} + {} = {}", a, b, c);
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
 }
