d289fc2e354d5eced7fe8f6edc73182b3b5e8170
trent-reed <164060782+trent-reed@users.noreply.github.com>
2024-08-07 14:47:51 -0700

    Separate unit tests into a different file when there are many of them (#83)
    This change will make it easier to manage our unit tests.
    
    There is nothing special about the `test` module in rust libraries, it's
    just a separate module that is conditionally compiled based on whether
    or not the crate is running with the `test` command. As such, it's
    possible (and allowable) to separate the unit test code into a separate
    file.
    
    It would probably be wise to move these tests closer to the code that
    they test, but let's do that in the future.
    
    fixes: #82

--- client/src/sessions/mod.rs
+++ client/src/sessions/mod.rs
@@ -45,18 +45,4 @@ impl Session for PasswordSession {
 }
 
 #[cfg(test)]
-mod tests {
-    use tpm2_rs_base::Tpm2bSimple;
-
-    use super::*;
-
-    #[test]
-    fn test_password_get_auth_command() {
-        let auth = Tpm2bAuth::from_bytes("hello".as_bytes()).unwrap();
-        let session = PasswordSession { auth };
-
-        let tpm_auth = session.get_auth_command();
-        assert_eq!(tpm_auth.session_handle, TpmiShAuthSession::RS_PW);
-        assert_eq!(auth, tpm_auth.hmac);
-    }
-}
+mod tests;
