1bb46fedbe96b8e65016bc2133a278fa50b93ac0
Trent Reed <trentreed@google.com>
2024-08-07 14:39:07 -0700

    Turns out we can actually use cfg at the top of an integration test file

--- client/tests/simulator_tests.rs
+++ client/tests/simulator_tests.rs
@@ -1,3 +1,15 @@
+#![cfg(feature = "tpm-simulator-tests")]
+
+/// These tests are enabled with the tpm-simulator-tests feature and only run against a TPM
+/// simulator. The docker container in this crate builds a simulator binary, or you may provide
+/// your own simulator binary by setting the TPM_RS_SIMULATOR env var for `cargo test` on the
+/// command line.
+///
+/// To run locally in the docker container:
+///  `cd client && docker compose run simulator_tests`
+///
+/// These tests must be run with `--test-threads=1`, because they use a single TCP port.
+
 use std::io::{Error, ErrorKind, IoSlice, Read, Result, Write};
 use std::net::TcpStream;
 use std::process::{Child, Command};
