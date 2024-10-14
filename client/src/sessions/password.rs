924c2b652b9aca5368d0d7cf94eaa39a66750c04
Ahmed <oddcoder@users.noreply.github.com>
2024-10-15 01:24:25 +0200

    Improve API for password sessions (#113)
    Idea originated from discussion at [1] to make the internal Tpm2bAuth
    inside a password session hidden. All users need to do is create a
    password and (possibly) fetch secrets stored inside. without having to
    worry about the internal marshalable structure required by TPM
    specifications.
    
    [1] https://github.com/tpm-rs/tpm-rs/pull/95#discussion_r1772048874
    
    ---------
    
    Signed-off-by: Ahmed Abdelraoof <ahmed.abdelraoof@huawei.com>

--- client/src/sessions/password.rs
+++ client/src/sessions/password.rs
@@ -1,14 +1,55 @@
 use crate::sessions::Session;
-use tpm2_rs_base::errors::{TssResult, TssTcsError};
+use tpm2_rs_base::errors::{TpmRcResult, TssResult, TssTcsError};
 use tpm2_rs_base::{
     Tpm2bAuth, Tpm2bNonce, Tpm2bSimple, TpmaSession, TpmiShAuthSession, TpmsAuthCommand,
     TpmsAuthResponse,
 };
 
 /// A password session.
+///
+/// # Usage:
+/// ```
+/// use tpm2_rs_client::sessions::PasswordSession;
+///
+/// let password1 = PasswordSession::new("hello world").unwrap(); // from a string
+/// let password2 = PasswordSession::new(&[1, 2, 3, 4, 5, 6]).unwrap(); // from a byte array
+///
+/// assert_eq!(password1.get_secret(), b"hello world");
+/// assert_eq!(password2.get_secret(), &[1, 2, 3, 4, 5, 6]);
+/// ```
 #[derive(Debug, PartialEq, Default)]
 pub struct PasswordSession {
-    pub auth: Tpm2bAuth,
+    auth: Tpm2bAuth,
+}
+
+impl PasswordSession {
+    /// This function creates a new password session using the
+    /// specified password.
+    ///
+    /// # Errors:
+    /// Returns [TpmRcError::Size](tpm2_rs_base::errors::TpmRcError::Size) if the
+    /// password size in bytes exceeds [`Tpm2bAuth::MAX_BUFFER_SIZE`].
+    ///
+    /// ```
+    /// use tpm2_rs_base::{errors::TpmRcError, Tpm2bAuth, Tpm2bSimple};
+    /// use tpm2_rs_client::sessions::PasswordSession;
+    ///
+    /// let bad_password = [0u8; Tpm2bAuth::MAX_BUFFER_SIZE + 1];
+    /// assert_eq!(
+    ///     PasswordSession::new(&bad_password).err().unwrap(),
+    ///     TpmRcError::Size
+    /// );
+
+    /// ```
+    pub fn new<T: AsRef<[u8]> + ?Sized>(password: &T) -> TpmRcResult<Self> {
+        Ok(PasswordSession {
+            auth: Tpm2bAuth::from_bytes(password.as_ref())?,
+        })
+    }
+    /// This function returns the data(password) stored inside a password session
+    pub fn get_secret(&self) -> &[u8] {
+        self.auth.get_buffer()
+    }
 }
 
 impl Session for PasswordSession {
