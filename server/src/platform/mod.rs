e43e28b42ead87c22471f0fb32e9ec5dfb12af0b
Ahmed Abdelraoof <ahmed.abdelraoof@huawei.com>
2024-10-24 11:01:50 +0200

    fixup! fixup! sever: Perform some renamings to improve clarity
    Signed-off-by: Ahmed Abdelraoof <ahmed.abdelraoof@huawei.com>

--- server/src/platform/mod.rs
+++ server/src/platform/mod.rs
@@ -5,6 +5,8 @@ pub use buffer::*;
 pub use crypto::*;
 
 /// Specifies all of the dependent types for [`TpmContext`].
+///
+/// [`TpmContext`]: crate::tpmctx::TpmContext
 pub trait TpmContextDeps {
     /// Interface to perform cryptographic operations.
     type Crypto: Crypto;
