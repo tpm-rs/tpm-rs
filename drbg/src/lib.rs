5c406c33868a11158a0f944eb3f3863cc568e015
Ahmed Abdelraoof <ahmed.abdelraoof@huawei.com>
2025-01-07 11:22:31 +0100

    fix spelling mistake
    Signed-off-by: Ahmed Abdelraoof <ahmed.abdelraoof@huawei.com>

--- drbg/src/lib.rs
+++ drbg/src/lib.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(test), no_std)]
 #![forbid(unsafe_code)]
 //! A Deterministic Random Bit Generator(DRBG) mechanism based
-//! on Hash Functions. Unless otherwise specified References to section
+//! on Hash Functions. Unless otherwise specified, references to section
 //! come directly from
 //! [NIST.SP.800-90Ar1](http://dx.doi.org/10.6028/NIST.SP.800-90Ar1).
 
